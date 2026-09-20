42f03a11165efdcd3cce6ed647ef4e50c771a68e
krizzyamando <krizzyleigh06@gmail.com>
2026-09-20 08:22:46 -0500

    Add files via upload

---
+++ PRELIM/OOPR/OLFUVMVJava.java
@@ -0,0 +1,28 @@
+package com.mycompany.olfuvmv.java;
+
+public class OLFUVMVJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("OUR LADY OF FATIMA UNIVERSITY");
+        System.out.println();
+
+        System.out.println("VISION");
+        System.out.println("A premier inclusive university of choice aspiring to improve man as man");
+        System.out.println("by developing individuals through a legacy of excellent education and");
+        System.out.println("compassionate value formation.");
+        System.out.println();
+
+        System.out.println("MISSION");
+        System.out.println("The Our Lady of Fatima University, together with Fatima Medical Science");
+        System.out.println("Foundation, Inc., is dedicated to the improvement of man as man through");
+        System.out.println("the holistic formation of individuals imbued with knowledge, skills, and virtues.");
+        System.out.println();
+
+        System.out.println("OLFU CORE VALUES");
+        System.out.println("R - Respect");
+        System.out.println("I - Integrity");
+        System.out.println("S - Service");
+        System.out.println("E - Excellence");
+    }
+}
